bd48fbe85d97a402d328e7134edc6ef67d8f30fc
Tofik Sahraoui <tofiksa@gmail.com>
2022-01-13 16:18:21 +0100

    lagt til ny background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Profile from './components/Profile'
+import { Spline } from 'react-spline'
+import SCENE_OBJECT from './scene.json'
+import FLOATIN_LAPTOP  from './image.jpg'
 
 function App() {
   return (
     <div className="App">
-      <Profile/>
-    </div>
+					<img src={FLOATIN_LAPTOP} alt='floating laptop'/>
+          <Profile/>
+		</div>
   );
 }
 
