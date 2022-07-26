f950d56727984a0714ffea6308d6aa075c92f3cb
Tofik Sahraoui <tofiksa@gmail.com>
2022-07-26 23:55:04 +0200

    latest

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import Profile from './components/Profile'
-import { Spline } from 'react-spline'
-import SCENE_OBJECT from './scene.json'
 import FLOATIN_LAPTOP  from './image.jpg'
 
 function App() {
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,12 +3,35 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+/**
+  * Implement your connectWallet method here
+  */
+ const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 class Profile extends Component {
+
+    
 
     render () {
 
         return (
+            {connectWallet}
             <Container>
                 <Row md={4}>
                     <Col><div class="neon-wrapper">
