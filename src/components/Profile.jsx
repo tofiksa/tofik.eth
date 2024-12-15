2db53f3a41eea77e039a9584dcddff5b8747f124
Tofik Sahraoui <tofiksa@gmail.com>
2024-12-15 23:28:58 +0100

    fixed errors from before

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -18,7 +18,7 @@ import Col from 'react-bootstrap/Col'
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
       console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]); 
+      //setCurrentAccount(accounts[0]); 
     } catch (error) {
       console.log(error)
     }
@@ -29,22 +29,22 @@ class Profile extends Component {
     
 
     render () {
-
         return (
-            {connectWallet}
-            <Container>
-                <Row md={4}>
-                    <Col><div class="neon-wrapper">
-                            <span class="txt" >tofik.eth</span>
-                            <span class="gradient"></span>
-                            <span class="dodge"></span>
-                        </div>
-                    </Col>
-                    <Col xs={6}>2 of 3</Col>
-                    <Col>3 of 3</Col>
-                </Row>
-                
-            </Container>
+            <>
+                <button type="button" onClick={connectWallet}>Connect Wallet</button>
+                <Container>
+                    <Row md={4}>
+                        <Col><div className="neon-wrapper">
+                                <span className="txt" >tofik.eth</span>
+                                <span className="gradient" />
+                                <span className="dodge" />
+                            </div>
+                        </Col>
+                        <Col xs={6}>2 of 3</Col>
+                        <Col>3 of 3</Col>
+                    </Row>                
+                </Container>
+            </>
         )
     }
 }
